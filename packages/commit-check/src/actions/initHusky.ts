0d0fc79709c651bba848c8cf78f29bbba74805bb
付翔 <449585662@qq.com>
2022-09-29 14:18:53 +0800

    tool(commit-check): 优化配置

--- packages/commit-check/src/actions/initHusky.ts
+++ packages/commit-check/src/actions/initHusky.ts
@@ -1,18 +1,14 @@
 import fs from 'fs';
 import path from 'path';
+import { execSync } from 'child_process';
 import { getPackageJson, rootPath } from '../utils.js';
-
-const getHuskyCommand = (hookName: string, command: string) => {
-  return `npx husky add .husky/${hookName} "${command}"`;
-};
-
-const commitMsgCommand = 'npx --no-install commitlint --edit $1';
-const preCommitCommand = 'npx lint-staged';
-const postinstall = `${getHuskyCommand('commit-msg', commitMsgCommand)} && ${getHuskyCommand('pre-commit', preCommitCommand)}`;
 
 export const initHusky = async () => {
   const pkgJson = getPackageJson();
   pkgJson.scripts['prepare'] = 'npx husky install';
-  pkgJson.scripts['postinstall'] = postinstall;
   fs.writeFileSync(path.resolve(rootPath, 'package.json'), JSON.stringify(pkgJson, null, 2));
+
+  execSync('npm run prepare');
+  execSync('npx husky add .husky/commit-msg "npx --no-install commitlint --edit $1"');
+  execSync('npx husky add .husky/pre-commit "npx lint-staged"');
 };
