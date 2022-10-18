15aca6f53dd74adc77dbe8d86fa18b6ad02255ff
付翔 <449585662@qq.com>
2022-10-18 14:18:29 +0800

    fix(hs-auth-tools): 修复打包工具

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -2,31 +2,15 @@
 import fs from 'fs';
 import path from 'path';
 import json from '@rollup/plugin-json';
-import postcss from 'rollup-plugin-postcss';
-import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import shebang from 'rollup-plugin-preserve-shebang';
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 // 公共插件配置
-const plugins = [
-  json(),
-  nodeResolve(),
-  postcss({
-    // 把 css 插入到 style 中
-    // inject: true,
-    // 把 css 放到和js同一目录
-    extract: true,
-  }),
-  babel({ babelHelpers: 'bundled' }),
-  typescript(),
-  commonjs(),
-  shebang(),
-];
+const plugins = [json(), nodeResolve(), typescript(), commonjs()];
 
 // 如果不是开发环境，开启压缩
 if (!isDev) plugins.push(terser());
