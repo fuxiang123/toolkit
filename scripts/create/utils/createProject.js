20d090a836226328150045595fea6b549e691aa9
付翔 <449585662@qq.com>
2022-11-11 11:09:33 +0800

    fix(root): 修复多级目录创建

--- scripts/create/utils/createProject.js
+++ scripts/create/utils/createProject.js
@@ -2,25 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const templateFile = require('template-file');
 
-module.exports = function (projectPath, templatePoints, templatePath) {
+module.exports = async (projectPath, templatePoints, templatePath) => {
   if (fs.existsSync(projectPath)) throw new Error('项目已存在');
 
-  fs.mkdirSync(projectPath);
+  fs.mkdirSync(projectPath, { recursive: true });
 
-  fs.readdirSync(templatePath).forEach(file => {
+  const filePaths = fs.readdirSync(templatePath);
+
+  for (let i = 0; i < filePaths.length; i++) {
+    const file = filePaths[i];
     // 处理tpl结尾的模板文件
     if (file.endsWith('.tpl')) {
       const filePath = path.resolve(templatePath, file);
       const targetPath = path.resolve(projectPath, file.replace('.tpl', ''));
-      console.log('targetPath', targetPath);
-      templateFile.renderFile(filePath, templatePoints).then(content => {
-        fs.writeFileSync(targetPath, content);
-      });
+      const content = await templateFile.renderFile(filePath, templatePoints);
+      fs.writeFileSync(targetPath, content);
     } else {
       // 非模板文件直接复制
       const filePath = path.resolve(templatePath, file);
       const targetPath = path.resolve(projectPath, file);
       fs.copyFileSync(filePath, targetPath);
     }
-  });
+  }
 };
