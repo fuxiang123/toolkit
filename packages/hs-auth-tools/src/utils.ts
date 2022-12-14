584319e94dcf843452b447c65b4e32af3b66d312
pengdongcheng <pengdongcheng.com>
2022-12-14 11:40:11 +0800

    fix(hs): 放开微信拦截

--- packages/hs-auth-tools/src/utils.ts
+++ packages/hs-auth-tools/src/utils.ts
@@ -1,11 +1,6 @@
 // 是否是微信客户端
 export function getIsWxClient() {
-  const ua = navigator.userAgent.toLowerCase();
-  const matchArr = ua.match(/MicroMessenger/i);
-  if (matchArr && matchArr.length > 0 && matchArr[0] === 'micromessenger') {
-    return true;
-  }
-  return false;
+  return true;
 }
 
 // 获取url中的参数
