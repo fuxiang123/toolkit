5337d02095f27bfda16ce4833f5d93081dad538c
付翔 <449585662@qq.com>
2023-01-13 10:16:20 +0800

    feat(axios): 导出axios

--- packages/requests/index.ts
+++ packages/requests/index.ts
@@ -2,5 +2,7 @@ import { setRequestConfig, requests, request, get, post, put, del, download } fr
 import { create } from './src/create';
 
 export type { RequestsConfig } from './src/initRequests';
+export * from 'axios';
+export { default as axios } from 'axios';
 export { create, request, get, post, put, del, download, setRequestConfig };
 export default requests;
