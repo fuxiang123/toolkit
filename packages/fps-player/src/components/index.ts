b1f8eec18988fdd12543c47c4d08bf9b1e104ad6
326 <2405825227@qq.com>
2023-04-10 11:55:13 +0800

    fix(fps-player): 更改导出方式

--- packages/fps-player/src/components/index.ts
+++ packages/fps-player/src/components/index.ts
@@ -2,3 +2,4 @@ import FpsPlayer from './fps-player/FpsPlayer.vue';
 import FpsPlayerController from './fps-player/FpsPlayerController';
 
 export { FpsPlayer, type FpsPlayerController };
+export default FpsPlayer;
