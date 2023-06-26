fe1e70108dea593ee32dfd63907400168361dfbc
bestbang <best_bang@163.com>
2023-06-26 14:53:40 +0800

    feat(添加验证码组件): 添加验证码组件
    添加验证码组件
    
    添加验证码组件

--- packages/saas/ui/src/Playground.vue
+++ packages/saas/ui/src/Playground.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // 测试页面，可以在该文件中测试组件
-import { AsyncButton } from './components'
+import { AsyncButton, Verify } from './components'
 import 'ant-design-vue/es/button/style/css'
 import { ref } from 'vue';
 
@@ -10,12 +10,26 @@ const asyncFn = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000))
   btnText.value = 'Hello World'
 }
+
+const verify = ref<typeof Verify>()
+
+// 显示图形验证码
+const showVerify = () => {
+  verify.value?.show();
+}
+
+// 图形验证码验证成功回调
+const capctchaCheckSuccess = async (params: any) => {
+  console.log(params);
+};
 </script>
 
 <template>
   <AsyncButton  type="primary" :async-click="asyncFn">
     {{btnText}}
   </AsyncButton>
+  <Verify ref="verify" :captcha-type="'blockPuzzle'" :img-size="{ width: '330px', height: '155px' }"
+      @success="capctchaCheckSuccess"></Verify>
 </template>
 
 <style scoped>
